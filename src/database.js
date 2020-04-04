3408f62309d5192ff655f20f0b9da91ca5d7056d
EduardoUnSuaste <emanuelun18@gmail.com>
2020-04-03 19:47:56 -0600

    Cambios
    estables

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 //Conexion de la Base de datos
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://u2lo1til284nhldz59me:pE68XUWt4LDCiIVtX9dz@byzor7cjlwwb9ri-mongodb.services.clever-cloud.com:27017/byzor7cjlwwb9ri',{
+mongoose.connect('mongodb://u2lo1til284nhldz59me:pE68XUWt4LDCiIVtX9dz@byzor7cjlwwb9ri-mongodb.services.clever-cloud.com:27017/byzor7cjlwwb9ri',
+{
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
